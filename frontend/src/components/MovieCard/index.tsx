abccbebe1b1b479a005b13d50677d14be247756b
claudiobelempb <claudio.c.lima@hotmail.com>
2022-01-16 09:52:26 -0300

    COMMIT: Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image';
 
-import ImgStarFull from 'assets/images/dsmovie/star-full.svg';
-import ImgStarHalf from 'assets/images/dsmovie/star-half.svg';
-import ImgStarEmpty from 'assets/images/dsmovie/star-empty.svg';
+import StarFull from 'assets/images/dsmovie/star-full.svg';
+import StarHalf from 'assets/images/dsmovie/star-half.svg';
+import StarEmpty from 'assets/images/dsmovie/star-empty.svg';
 import { ButtonDefault } from 'components/ButtonDefault';
 
 import styles from './styles.module.scss';
 import { Movie } from 'types/Movie';
+import { MovieStar } from 'components/MovieStar';
 
 type MovieCardProps = {
   movie: Movie;
+  score?: number;
 };
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  // const stars = [StarFull, StarHalf, StarEmpty];
+
   return (
     <>
       <article className={`${styles.movie__container_card}`}>
@@ -21,11 +25,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
           <h2>{movie.title}</h2>
           <span>{movie.score > 0 ? movie.score.toFixed(1) : '-'}</span>
           <div className={styles.movie__star}>
-            <Image src={ImgStarFull} alt={'Imagem do Card'} />
-            <Image src={ImgStarFull} alt={'Imagem do Card'} />
-            <Image src={ImgStarFull} alt={'Imagem do Card'} />
-            <Image src={ImgStarHalf} alt={'Imagem do Card'} />
-            <Image src={ImgStarEmpty} alt={'Imagem do Card'} />
+            <MovieStar score={movie.score} />
           </div>
           <strong>{movie.count} avaliações</strong>
           <ButtonDefault
