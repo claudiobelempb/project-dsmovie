6d703f32ea3c3792477cfc6a5127d5071b7ef32b
claudiobelempb <claudio.c.lima@hotmail.com>
2022-01-16 21:18:26 -0300

    COMMIT: Save, build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,22 +2,7 @@
 module.exports = {
   reactStrictMode: true,
 
-  async redirects() {
-    return [
-      {
-        source: '/valuation/:id',
-        destination: '/',
-        permanent: false
-      }
-    ];
-  },
-
   images: {
     domains: ['localhost', 'https://www.themoviedb.org', 'www.themoviedb.org']
-  },
-
-  env: {
-    //{process.env.dbPath}
-    dbPath: 'mysql'
   }
 };
